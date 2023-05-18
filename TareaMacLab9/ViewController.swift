4e58464be11b95c2a07a0e7ed2899fa2a07d5533
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-05-18 09:04:02 -0500

    Initial Commit

---
+++ TareaMacLab9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TareaMacLab9
+//
+//  Created by Mac 16 on 18/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
